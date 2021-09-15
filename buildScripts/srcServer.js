9c31efb3bf27ecc7d0364d0ba76128867efd656c
JeremyWarui <jerywarne7@gmail.com>
2021-09-15 17:22:55 +0300

    webpackbundler

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,15 +1,22 @@
 import express from "express";
 import { join } from "path";
 import open from "open";
-
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    publicPath: config.output.publicPath,
+  })
+);
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "../src/index.html"));
 });
-
 
 app.listen(port, (err) => {
   if (err) {
